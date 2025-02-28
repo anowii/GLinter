15f5f5ca9f3aefc431ede5a8a0ff9857bbfb8e47
anowii <91365639+anowii@users.noreply.github.com>
2025-02-28 11:07:07 +0100

    trying to fix gitleaks

--- run_gitleaks.bat
+++ run_gitleaks.bat
@@ -1,4 +1,4 @@
+::version::gitleaks_8.24.0_windows_x64
 @echo off
 REM Run Gitleaks against the specified directory
-tools\gitleaks_8.24.0_windows_x64\gitleaks.exe  dir -v %1
-
+tools\gitleaks_windows\gitleaks.exe detect -v --report-path gitleaks-report.json --source %1
